e8cbaec1c1960b0eb6bb49296ac5bddce627f5d1
almatairi <almtahirii@gmail.com>
2023-11-28 02:31:48 +0100

    last changes

--- src/main/java/mk/finki/ukim/wp/lab/web/bookDetails.java
+++ src/main/java/mk/finki/ukim/wp/lab/web/bookDetails.java
@@ -5,30 +5,34 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import lombok.AllArgsConstructor;
-import mk.finki.ukim.wp.lab.service.AuthorService;
+import mk.finki.ukim.wp.lab.model.Book;
 import mk.finki.ukim.wp.lab.service.BookService;
+import org.thymeleaf.context.IWebContext;
 import org.thymeleaf.context.WebContext;
 import org.thymeleaf.spring6.SpringTemplateEngine;
 import org.thymeleaf.web.IWebExchange;
 import org.thymeleaf.web.servlet.JakartaServletWebApplication;
 
 import java.io.IOException;
+import java.util.Optional;
 
-@AllArgsConstructor
 @WebServlet(name = "book-details", urlPatterns = "/bookDetails")
 public class bookDetails extends HttpServlet {
     private final SpringTemplateEngine springTemplateEngine;
     private final BookService bookService;
 
+    public bookDetails(SpringTemplateEngine springTemplateEngine, BookService bookService) {
+        this.springTemplateEngine = springTemplateEngine;
+        this.bookService = bookService;
+    }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         IWebExchange webExchange = JakartaServletWebApplication.buildApplication(getServletContext()).buildExchange(req, resp);
 
-        WebContext context = new WebContext(webExchange);
+        WebContext context = new WebContext(webExchange, req.getLocale());
 
-        context.setVariable("books", bookService.listBooks());
+        context.setVariable("book", bookService.listBooks());
 
         springTemplateEngine.process(
                 "bookDetails.html",
@@ -36,5 +40,6 @@ public class bookDetails extends HttpServlet {
                 resp.getWriter()
         );
     }
+
 
 }
